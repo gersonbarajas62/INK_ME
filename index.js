172fce5d36bf0ac8c1125c491d5088ec5d3f0f1e
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-06-28 14:56:11 -0500

    fixed bugs and connected front end to back end

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+
 const express = require("express");
-const path = require("path"); 
-const app = express();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
-const bodyParser = require("body-parser")
+const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
 
 app.use(express.static(path.join(__dirname, "public")));
+
+
 app.listen(port);
+
 
 module.export = app
